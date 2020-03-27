04b04c2c2184476c7c20d774b042efdbb845c8b4
vmipfdr <paynemw@yahoo.com>
2020-03-27 13:39:19 -0400

    the base url redirects are wrong, but now the base.html shows up at the root

--- ghost/urls.py
+++ ghost/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.customer_list, name='customer_list'),
+    path('', views.base, name='base'),
+    path('customers', views.customer_list, name='customer_list'),
     path('customers/<int:pk>', views.customer_detail, name='customer_detail'),
     path('customers/create', views.customer_create, name='customer_create'),
     path('customers/<int:pk>/edit', views.customer_edit, name='customer_edit'),
