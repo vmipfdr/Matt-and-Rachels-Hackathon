d5474daa65b422759787ceaff3e41ae90694b08d
vmipfdr <paynemw@yahoo.com>
2020-03-27 15:43:32 -0400

    added some placeholders and attempted onsubmit

--- ghost/forms.py
+++ ghost/forms.py
@@ -7,3 +7,10 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ('first', 'last', 'email')
+        widgets = {
+            'first': forms.TextInput(attrs={'placeholder': 'John'}),
+            'last': forms.TextInput(attrs={'placeholder': 'Doe'}),
+            'email': forms.TextInput(attrs={
+                'placeholder': 'youremail@here.com'
+            }),
+        }
