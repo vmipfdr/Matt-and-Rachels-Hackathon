b50ad1bcd82264a6e295ef996eb7a61eaf4c40cb
RachelIsrael <racheleisrael4@gmail.com>
2020-03-27 10:49:55 -0400

    updated model

--- ghost/views.py
+++ ghost/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Post, Comment
+from .models import Customer
 from .forms import CustomerForm
 from django.contrib.auth.decorators import login_required
 
