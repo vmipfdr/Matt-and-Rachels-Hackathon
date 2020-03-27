5f01386f814eeb8cef4c8de8c829fa944ef3a9a7
vmipfdr <paynemw@yahoo.com>
2020-03-27 10:48:17 -0400

    added some views, models and forms

--- ghost/views.py
+++ ghost/views.py
@@ -1,4 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+from .forms import CustomerForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+# this function is first getting the data then it's going to render it in this HTML file
+# creating a variable for Artists.objects.all() will make our process cleaner
+#
+
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, 'ghost/customer_list.html', {'customers': customers})
+
+
+# def post_detail(request, pk):
+#     post = Post.objects.get(id=pk)
+#     # comments = Comment.object.all(id=fk)
+#     return render(request, 'scribble/post_detail.html', {'post': post})
+
+
+# def post_detail(request, pk):
+#     post = Post.objects.get(id=pk)
+#     return render(request, 'scribble/post_detail.html', {'post': post})
+
+def customer_detail(request, pk):
+    customers = Customer.objects.filter(customer=pk)
+    customer = Customer.objects.get(id=pk)
+    return render(request, 'ghost/customer_detail.html', {'customer': customer})
+
+
+@login_required
+def customer_create(request):
+    if request.method == "POST":
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('customer_list')
+    form = PostForm()
+    return render(request, 'ghost/customer_create.html', {'form': form})
